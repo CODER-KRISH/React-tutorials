92abd53ed3aa21e4e9669fa47e15aeee2c41b727
monster-KP <kaprajapati2495@gmail.com>
2025-11-05 15:04:12 +0530

    first commit

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+      <div className="Parent">
+        <h1>Hello I am Parent</h1>
+      </div>
+      <div className="child">
+        <h2>Hello I am Child</h2>
+      </div>
+      <div className="grandChild">
+        <h3>Hello I am grandChild</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
